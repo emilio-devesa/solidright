774bdc64e5b2d530d9d611df8db10ddf702b24ab
"Yudaisy Ramos Díaz" <"yudaramos25@gmail.com">
2022-03-24 12:32:49 +0100

    feat: Create methods showArtworkDetails, changeNameArtwork, changePriceArtwork

--- contracts/solidright.sol
+++ contracts/solidright.sol
@@ -2,8 +2,8 @@
 pragma solidity >=0.8.0 <=0.8.13;
 
 // Imports
-
-contract solidRight {
+import 
+contract solidRight is Owner {
 
     struct Artwork {
         string name;
@@ -18,14 +18,26 @@ contract solidRight {
     mapping(address => uint) artworkCounter;
 
     // Events
-    event newObraCreated(uint id, string name);
+    event newObraCreated(uint id, string name);    
 
-    function createArtwork(string memory _name, uint256 _price) private {
+    function createArtwork(string memory _name, uint256 _price, bytes32[] _fileHashes) private {
         // Id equals to the entry index on the array.
-        uint id = artworks.push(Obra(_name, _price)) - 1;
+        uint id = artworks.push(Obra(_name, _price, _fileHashes)) - 1;
         // Assign the ownership of the obra to msg.sender and increase it's counter by one.
         artworkOwnership[id] = msg.sender;
         artworkCounter[msg.sender]++;
+    }
+
+    function showArtworkDetails(uint _ArtworkId) external view returns(Artwork){
+       return artworks[_ArtworkId];
+    }
+
+    function changeNameArtwork(uint _ArtworkId, string memory _newName) public onlyOwner {
+        artwork[_ArtworkId].name = newName;
+    }
+
+    function changePriceArtwork(uint _ArtworkId, uint memory _newPrice) public onlyOwner {
+        artwork[_ArtworkId].price = newPrice;
     }
 
 }
